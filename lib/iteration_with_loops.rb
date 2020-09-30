e93655a3dedd83659e04b83a3dd31fde9f5e20dc
michael james <majames071@gmail.com>
2020-09-30 04:03:49 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,10 +2,11 @@ def join_nested_strings(src)
 
 
 src.flatten!
-y = src-(-1..9).to_a
+y = src-(-1..1000).to_a
 y.join (" ")
 
 
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
+
 end
